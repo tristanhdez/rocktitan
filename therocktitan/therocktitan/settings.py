1ce99796e9549a4e30ebe1028a8b94aa253eeeba
Tristan <christian.hflores@alumnos.udg.mx>
2022-05-25 17:14:43 -0500

    xc

--- therocktitan/therocktitan/settings.py
+++ therocktitan/therocktitan/settings.py
@@ -141,8 +141,3 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
