1e480ac5d593c8b01113c63f6a26b22a0c6f57bc
Tristan <christian.hflores@alumnos.udg.mx>
2022-05-25 17:10:19 -0500

    fixed bug

--- therocktitan/therocktitan/wsgi.py
+++ therocktitan/therocktitan/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kloudless.settings")
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'therocktitan.settings')
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
